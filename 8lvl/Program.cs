2d722462f930815ece8552dfa98bb70bf250e657
Pavlo Volodymyrovych <126395347+PavloVolodymyrovych@users.noreply.github.com>
2023-03-05 14:43:01 +0200

    Add files via upload

---
+++ 8lvl/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace You_Only_Need_One_Beginner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Test one: {Check(new object[] { 't', 'e', 's', 't' }, 'e')}");
+            Console.WriteLine($"Test two: {Check(new object[] { 80, 117, 115, 104, 45, 85, 112, 115 }, 45)}");
+            Console.WriteLine($"Test three: {Check(new object[] { "what", "a", "great", "kata" }, "kat")}");
+        }
+        public static bool Check(object[] a, object x)
+        {
+            return a.Contains(x);
+        }
+    }
+}
