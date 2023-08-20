f178cc504ee2568ebda4470f40d6418553a51376
Pavlo Volodymyrovych <126395347+PavloVolodymyrovych@users.noreply.github.com>
2023-08-20 12:49:24 +0300

    Add files via upload

---
+++ 6lvl/Break_Camel_Case.cs
@@ -0,0 +1,21 @@
+﻿/*Complete the solution so that the function will break up camel casing, using a space between words.*/
+
+Console.WriteLine(BreakCamelCase("camelCasing"));
+
+static string BreakCamelCase(string str)
+{
+    // complete the function
+    string value = "";
+    foreach (char c in str)
+    {
+        if (char.IsUpper(c))
+        {
+            value += (" " + c); 
+        }
+        else
+        {
+            value += c;
+        }
+    }
+    return value;
+}
