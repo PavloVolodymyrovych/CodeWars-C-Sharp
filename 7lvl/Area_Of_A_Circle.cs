9ea3b4fc61663951ed85a09b6920e7c0107616d0
Pavlo Volodymyrovych <126395347+PavloVolodymyrovych@users.noreply.github.com>
2023-09-11 14:28:57 +0300

    Add files via upload

---
+++ 7lvl/Area_Of_A_Circle.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(CalculateAreaOfCircle(68.0)); //
+static double CalculateAreaOfCircle(double radius)
+{
+    if (radius > 0)
+    {
+        return Math.PI * (radius * radius);
+    }
+    else
+    {
+        throw new ArgumentException("ArgumentException");
+    }
+}
